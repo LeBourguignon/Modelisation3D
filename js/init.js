b8a3432edae206aae0b43cf9a5e1fa98682426e7
tom-rh <64775524+tom-rh@users.noreply.github.com>
2023-02-06 19:53:25 +0100

    creation repere

--- js/init.js
+++ js/init.js
@@ -12,11 +12,11 @@ function init(){
  rendu.setSize(window.innerWidth*.9, window.innerHeight*.9);
  cameraLumiere(scene,camera);
  lumiere(scene);
- //repere(scene);
+ repere(scene);
  
-    var axes = new THREE.AxesHelper(1);    
+    var axes = new THREE.AxesHelper(1);
     scene.add(axes);
-    //repere(scene)
+    repere(scene);
 
  //********************************************************
  //
@@ -70,3 +70,26 @@ function init(){
   }
  
 } // fin fonction init()
+
+function vecteur(MaScene, A, B, CoulHexa, longCone, RayonCone) {
+	// MaScene : objet de type THREE.Scene
+	// A, B : objets de type THREE.Vector3
+	// CoulHexa : couleur du vecteur en hexadécimal
+	// longCone : longueur du cone
+	// RayonCone : rayon maximal du cone
+	// Création du vecteur AB
+	let AB = new THREE.Vector3();
+	AB.subVectors(B, A);
+	// Création de la flèche du vecteur AB
+	let flecheAB = new THREE.ArrowHelper(AB.clone().normalize(), A, AB.length(), CoulHexa, longCone, RayonCone);
+	// Ajout de la flèche du vecteur AB à la scène
+	MaScene.add(flecheAB);
+  }// fin function vecteur()
+  
+  function repere(MaScene) {
+	// MaScene : objet de type THREE.Scene
+	// Création des axes du repère
+	vecteur(MaScene, new THREE.Vector3(0, 0, 0), new THREE.Vector3(1, 0, 0), 0xFF0000, 0.1, 0.05);
+	vecteur(MaScene, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 1, 0), 0x00FF00, 0.1, 0.05);
+	vecteur(MaScene, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 1), 0x0000FF, 0.1, 0.05);
+  }// fin function repere()
