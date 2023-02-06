4f4f1386b70ec6492422dc2718d0c1312b1f21e2
tom-rh <64775524+tom-rh@users.noreply.github.com>
2023-02-06 20:15:36 +0100

    fix indentation

--- js/init.js
+++ js/init.js
@@ -1,31 +1,28 @@
-
-
 function init(){
- var stats = initStats();
+	var stats = initStats();
     // creation de rendu et de la taille
- let rendu = new THREE.WebGLRenderer({ antialias: true });
- rendu.shadowMap.enabled = true;
- let scene = new THREE.Scene();   
- let camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 100);
- rendu.shadowMap.enabled = true;
- rendu.setClearColor(new THREE.Color(0xFFFFFF));
- rendu.setSize(window.innerWidth*.9, window.innerHeight*.9);
- cameraLumiere(scene,camera);
- lumiere(scene);
- repere(scene);
+	let rendu = new THREE.WebGLRenderer({ antialias: true });
+	rendu.shadowMap.enabled = true;
+	let scene = new THREE.Scene();   
+	let camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 100);
+	rendu.shadowMap.enabled = true;
+	rendu.setClearColor(new THREE.Color(0xFFFFFF));
+	rendu.setSize(window.innerWidth*.9, window.innerHeight*.9);
+	cameraLumiere(scene,camera);
+	lumiere(scene);
+	repere(scene);
  
-    var axes = new THREE.AxesHelper(1);
-    scene.add(axes);
-    repere(scene);
+	var axes = new THREE.AxesHelper(1);
+	scene.add(axes);
+	repere(scene);
 
- //********************************************************
- //
- //  P A R T I E     G E O M E T R I Q U E
- //
- //********************************************************
+	//********************************************************
+	//
+	//  P A R T I E     G E O M E T R I Q U E
+	//
+	//********************************************************
  
 	// Création de la sphère S : centre O, rayon 5
-
 	var sphereGeometry = new THREE.SphereGeometry(5, 32, 32);
 	var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x0000FF });
 	var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
@@ -37,53 +34,53 @@ function init(){
 	var smallSphere = new THREE.Mesh(smallSphereGeometry, smallSphereMaterial);
 	smallSphere.position.set(sphere.position.x + 5, sphere.position.y, sphere.position.z);
 	scene.add(smallSphere);
-	  
- 
- //********************************************************
- //
- // F I N      P A R T I E     G E O M E T R I Q U E
- //
- //********************************************************
- 
- //********************************************************
- //
- //  D E B U T     M E N U     G U I
- //
- //********************************************************
 
- //********************************************************
- //
- //  F I N     M E N U     G U I
- //
- //********************************************************
- renduAnim();
+ 
+	//********************************************************
+	//
+	// F I N      P A R T I E     G E O M E T R I Q U E
+	//
+	//********************************************************
+
+	//********************************************************
+	//
+	//  D E B U T     M E N U     G U I
+	//
+	//********************************************************
+
+	//********************************************************
+	//
+	//  F I N     M E N U     G U I
+	//
+	//********************************************************
+ 	renduAnim();
  
  
-  // ajoute le rendu dans l'element HTML
- document.getElementById("webgl").appendChild(rendu.domElement);
-   
-  // affichage de la scene
- rendu.render(scene, camera);
-  
- 
- function reAffichage() {
-  setTimeout(function () { 
- 
-  }, 200);// fin setTimeout(function ()
-    // render avec requestAnimationFrame
-  rendu.render(scene, camera);
- }// fin fonction reAffichage()
- 
- 
-  function renduAnim() {
-    stats.update();
-    // render avec requestAnimationFrame
-    requestAnimationFrame(renduAnim);
-// ajoute le rendu dans l'element HTML
-    rendu.render(scene, camera);
-  }
- 
-} // fin fonction init()
+	// ajoute le rendu dans l'element HTML
+	document.getElementById("webgl").appendChild(rendu.domElement);
+	
+	// affichage de la scene
+	rendu.render(scene, camera);
+	
+	
+	function reAffichage() {
+		setTimeout(function () { 
+		
+		}, 200);// fin setTimeout(function ()
+		// render avec requestAnimationFrame
+		rendu.render(scene, camera);
+	}// fin fonction reAffichage()
+	
+	
+	function renduAnim() {
+		stats.update();
+		// render avec requestAnimationFrame
+		requestAnimationFrame(renduAnim);
+		// ajoute le rendu dans l'element HTML
+		rendu.render(scene, camera);
+	}
+	
+	} // fin fonction init()
 
 function vecteur(MaScene, A, B, CoulHexa, longCone, RayonCone) {
 	// MaScene : objet de type THREE.Scene
